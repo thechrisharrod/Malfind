9ee80b8aa9c859fa24fc468027a6dde08e4cc08f
thechrisharrod <chris.harrod@gmail.com>
2015-10-08 13:31:02 -0400

    Bugfix

--- Maltrieve.ps1
+++ Maltrieve.ps1
@@ -95,9 +95,7 @@ foreach($Item in $Content){
         $arrItems += $Item
     }
 }
-foreach ($Object in $arrItems){
-    $ObjectPath = $Object.description.split(" ").replace(",","")[1]
-    $ObjectURL = "http://$ObjectPath"
+foreach ($ObjectURL in $arrItems){
     if($arrCompleted -notcontains $ObjectURL){
         Write-Host $ObjectURL
         try{
