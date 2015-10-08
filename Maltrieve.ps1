1f917ca3bb4df2437a04973285d40905692f350f
thechrisharrod <chris.harrod@gmail.com>
2015-10-08 10:56:39 -0400

    .

--- Maltrieve.ps1
+++ Maltrieve.ps1
@@ -1,20 +1,101 @@
-﻿$Session = New-PSSession client
+﻿# Author: Chris Harrod
+#
+# Downloads malware samples from the following sources:
+# malc0de.com
+# zeustracker.abuse.ch
+# malwaredomainlist.com
+# vxvault.net
+# support.clean-mx.de
 
 
+$OutputDirectory = "C:\Scripting\PoSH-Maltrieve\Output"
 
 [xml]$RSS = Invoke-WebRequest "http://malc0de.com/rss/"
 [xml]$RSS = $RSS.InnerXml
-$Items = $rss.rss.channel.item
-foreach ($Item in $Items){
-    $item.description.split(" ").replace(",","")[1]
+$arrItems = $rss.rss.channel.item
+foreach ($Object in $arrItems){
+    $ObjectPath = $Object.description.split(" ").replace(",","")[1]
+    $ObjectURL = "http://$ObjectPath"
+    $File = Invoke-Webrequest $ObjectURL -OutFile "$OutputDirectory/temp.temp" -PassThru
+    $MD5 = (Get-FileHash $OutputDirectory/temp.temp -Algorithm MD5).Hash
+    Rename-Item $OutputDirectory/temp.temp $MD5
 }
 
 
+[xml]$RSS = Invoke-WebRequest "https://zeustracker.abuse.ch/monitor.php?urlfeed=binaries"
+[xml]$RSS = $RSS.InnerXml
+$arrItems = $rss.rss.channel.item
+foreach ($Object in $arrItems){
+    $ObjectURL = $Object.description.split(" ").replace(",","")[1]
+    $File = Invoke-Webrequest $ObjectURL -OutFile "$OutputDirectory/temp.temp" -PassThru
+    $MD5 = (Get-FileHash $OutputDirectory/temp.temp -Algorithm MD5).Hash
+    Rename-Item $OutputDirectory/temp.temp $MD5
+}
 
-"https://zeustracker.abuse.ch/monitor.php?urlfeed=binaries"
-"http://www.malwaredomainlist.com/hostslist/mdl.xml"
-"http://malc0de.com/rss/"
-"http://vxvault.net/URL_List.php"
-"http://urlquery.net/"
-"http://support.clean-mx.de/clean-mx/rss?scope=viruses&limit=0%2C64"
-"http://malwareurls.joxeankoret.com/normal.txt"
+[xml]$RSS = Invoke-WebRequest "http://www.malwaredomainlist.com/hostslist/mdl.xml"
+[xml]$RSS = $RSS.InnerXml
+$arrItems = $rss.rss.channel.item
+foreach ($Object in $arrItems){
+    
+    $ObjectPath = $Object.description.split(" ").replace(",","")[1]
+    $ObjectURL = "http://$ObjectPath"
+    try{
+        $File = Invoke-Webrequest $ObjectURL -OutFile "$OutputDirectory/temp.temp" -PassThru
+    }
+    catch{}
+    if($File){
+        $MD5 = (Get-FileHash $OutputDirectory/temp.temp -Algorithm MD5).Hash
+        if(!(Test-Path $MD5)){
+            Rename-Item $OutputDirectory/temp.temp $MD5
+        }
+    }
+    $File = $Null
+}
+
+$Content = (Invoke-WebRequest "http://vxvault.net/URL_List.php").Content.Split('')
+$arrItems = @()
+foreach($Item in $Content){
+    if ($Item -like "*http://*"){
+        $arrItems += $Item
+    }
+}
+foreach ($Object in $arrItems){
+    $ObjectPath = $Object.description.split(" ").replace(",","")[1]
+    $ObjectURL = "http://$ObjectPath"
+    try{
+        $File = Invoke-Webrequest $ObjectURL -OutFile "$OutputDirectory/temp.temp" -PassThru
+    }
+    catch{}
+    if($File){
+        $MD5 = (Get-FileHash $OutputDirectory/temp.temp -Algorithm MD5).Hash
+        if(!(Test-Path $MD5)){
+            Rename-Item $OutputDirectory/temp.temp $MD5
+        }
+    }
+    $File = $Null
+}
+
+
+[xml]$RSS = (Invoke-WebRequest "http://support.clean-mx.de/clean-mx/rss?scope=viruses&limit=0%2C64")
+[xml]$RSS = $RSS.InnerXml
+$Content = $rss.rss.channel.item
+$arrItems = @()
+foreach($Item in $Content){
+    $arrItems += $Item.title.'#cdata-section'
+}
+
+foreach ($Object in $arrItems){
+    $ObjectPath = $Object.description.split(" ").replace(",","")[1]
+    $ObjectURL = "http://$ObjectPath"
+    try{
+        $File = Invoke-Webrequest $ObjectURL -OutFile "$OutputDirectory/temp.temp" -PassThru
+    }
+    catch{}
+    if($File){
+        $MD5 = (Get-FileHash $OutputDirectory/temp.temp -Algorithm MD5).Hash
+        if(!(Test-Path $MD5)){
+            Rename-Item $OutputDirectory/temp.temp $MD5
+        }
+    }
+    $File = $Null
+}
